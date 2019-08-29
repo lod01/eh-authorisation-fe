9ae190c671ff6df8bd1be1ec14c3159a143dffa3
Dominic McAdden <dominic.mcadden@lloydsonlinedoctor.com>
2019-08-29 16:14:04 +0100

    UI tidy

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   exports: [
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
