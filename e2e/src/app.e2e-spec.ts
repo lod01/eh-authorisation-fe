a0f9fcf110a6248f4c09b491a62a0455ef2151ed
Dominic McAdden <dominic.mcadden@lloydsonlinedoctor.com>
2019-08-30 10:23:21 +0100

    getting all unit and e2e tests working

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to eh-authorisation-fe!');
+    expect(page.getTitleText()).toEqual('Authorisation');
   });
 
   afterEach(async () => {
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {MaterialModule} from '../material/material.module';
+import {ActivatedRoute} from '@angular/router';
+import {ExternalUrl} from '../external-url/external-url';
+import {of} from 'rxjs';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  const url: string = 'http%3A%2F%2Fonlinedoctor.lloydspharmacy.com%2Fmock';
+  const mockActivatedRoute: any = {
+    paramMap: of({params: {url}})
+  };
+  const mockExternalUrl: ExternalUrl = jasmine.createSpyObj<ExternalUrl>(['navigate']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ MaterialModule, NoopAnimationsModule ],
+      declarations: [ LoginComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute},
+        { provide: ExternalUrl, useValue: mockExternalUrl }
+      ]
     })
     .compileComponents();
   }));
